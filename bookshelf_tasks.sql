6b7eb4c8bfd6db82a0c9dd8cb7085d629c209c98
qweasads <126321177+qweasads@users.noreply.github.com>
2023-06-14 13:35:39 +0300

    Add files via upload

--- bookshelf_tasks.sql
+++ bookshelf_tasks.sql
@@ -1,4 +1,4 @@
--- 1. 
+-- 1. Найти автора с самым большим числом книг и вывести его имя
 SELECT authors.name as 'Автор', COUNT(authors.name) as count  FROM authors
 JOIN authors_books ON authors_books.authors_id = authors.id
 JOIN books ON authors_books.books_id = books.id
@@ -6,52 +6,55 @@ GROUP BY authors.name
 ORDER BY count DESC
 LIMIT 1
 ;
--- 2. 
+-- 2. Вывести пять самых старых книг у которых указан год издания
 SELECT * FROM books
 WHERE year LIKE '%%'
 ORDER BY year ASC
 LIMIT 5
 ;
--- 3. 
+-- 3. Вывести общее количество книг на полке в кабинете
 SELECT COUNT(shelves.title) as 'Количество книг' FROM shelves
 JOIN books ON shelves.id = books.shelves_id
-WHERE shelves.title LIKE '%кабин%'
+WHERE shelves.title = 'Полка в кабинете'
 ;
--- 4. 
+-- 4. Вывести названия, имена авторов и годы издания книг, которые находятся на полке в спальне
 SELECT books.title as 'Название', authors.name as 'Автор', books.year as 'Год', shelves.title 'Полка' FROM shelves
 JOIN books ON shelves.id = books.shelves_id
 JOIN authors_books ON books.id = authors_books.books_id
 JOIN authors ON authors_books.authors_id = authors.id
-WHERE shelves.title LIKE '%спал%'
+WHERE shelves.title = 'Полка в спальне'
 ;
--- 5. 
+-- 5. Вывести названия и годы издания книг, написанных автором 'Лев Толстой'
 SELECT books.title as 'Книга', books.year 'Год', authors.name as 'Автор' FROM books
 JOIN authors_books ON books.id = authors_books.books_id
 JOIN authors ON authors_books.authors_id = authors.id
 WHERE authors.name = 'Лев Толстой'
 ;
--- 6. 
+-- 6. Вывести название книг, которые написали авторы, чьи имена начинаются на букву "А"
 SELECT books.title as 'Книга', authors.name as 'Автор' FROM books
 JOIN authors_books ON books.id = authors_books.books_id
 JOIN authors ON authors_books.authors_id = authors.id
 WHERE authors.name LIKE 'А%'
 ;
--- 7. 
+-- 7. Вывести название книг и имена авторов для книг, которые находятся на полках, названия которых включают слова «верхняя» или «нижняя»
 SELECT books.title as 'Книга', authors.name as 'Автор', shelves.title as 'Полка' FROM books
 JOIN authors_books ON books.id = authors_books.books_id
 JOIN authors ON authors_books.authors_id = authors.id
 JOIN shelves ON books.shelves_id = shelves.id
 WHERE 
-	(shelves.title LIKE 'верхняя%') 
+    (shelves.title LIKE 'верхняя%') 
     OR 
     (shelves.title LIKE 'нижняя%')
 ;
--- 8. 
+-- 8. Книгу «Божественная комедия» автора «Данте Алигьери» одолжили почитать другу Ивану Иванову, необходимо написать один или несколько запросов которые отразят это событие в БД
 UPDATE books
-SET books.friends_id = 1
-WHERE id = 9
+JOIN authors_books ON authors_books.books_id = books.id
+JOIN authors ON authors_books.authors_id = authors.id
+SET books.friends_id = (SELECT friends.id FROM friends WHERE friends.name = 'Иванов Иван')
+WHERE authors.name = 'Данте Алигьери' AND books.title = 'Божественная комедия'
+and books.id > 0
 ;
--- 9. 
+-- 9. Добавить в базу книгу «Краткие ответы на большие вопросы», год издания 2020, автор «Стивен Хокинг», положить ее на полку в кабинете
 INSERT INTO books (books.title, books.year, books.shelves_id)
 VALUES ('Краткие ответы на большие вопросы', '2020', 1)
 ;
